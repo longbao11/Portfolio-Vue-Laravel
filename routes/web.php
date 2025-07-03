9ae0082530f88afb10f0e98c1ab07f4b186b6a5f
longbao11 <longbao572@gmail.com>
2025-07-03 11:35:35 +0800

    add test route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/contact', [FeedbackController::class, 'store'])->name('feedback.store');
 
     Route::get('/admin/messages', [FeedbackController::class, 'index'])->name('feedback.index');
+
+
 });
+
+Route::get('/test', fn() => 'It works!');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
